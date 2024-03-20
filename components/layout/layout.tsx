ff84ae7a2a96527fbfdcdd33c421e474f9d0f1b6
DanialSobri <adanial091@gmail.com>
2024-03-20 19:00:57 +0800

    Feat: Add loading

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -4,12 +4,15 @@ import { NavbarWrapper } from '../navbar/navbar';
 import { SidebarWrapper } from '../sidebar/sidebar';
 import { SidebarContext } from './layout-context';
 import { WrapperLayout } from './layout.styles';
+import { useRouter } from 'next/router';
 
 interface Props {
    children: React.ReactNode;
 }
 
 export const Layout = ({ children }: Props) => {
+   const [loading, setLoading] = React.useState(true)
+   const router = useRouter()
    const [sidebarOpen, setSidebarOpen] = React.useState(false);
    const [_, setLocked] = useLockedBody(false);
    const handleToggleSidebar = () => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,35 @@
 import type {NextPage} from 'next';
 import Content from '../components/home/content';
+import { Suspense, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import useAuth from '../components/hooks/useAuth';
+import Loading from '../components/layout/Loading';
 
 const Home: NextPage = () => {
 
-   // <Content />  
-   return <></>;
+   const [loading, setLoading] = useState(true)
+   const router = useRouter()
+ 
+   // Check if user is authenticated
+   const user = useAuth()
+ 
+   // Redirect to login page if not authenticated
+   useEffect(() => {
+     if (!user?.username) {
+       router.push(process.env.PROXY_PATH+'/login')
+     } else {
+       setLoading(false)
+     }
+   }, [])
+ 
+   // Show loading component while checking
+   if (loading) {
+     return <Loading />
+   }
+ 
+   // Render page content if authenticated
+   return <div>Welcome, {user?.username}</div>
 };
 
 export default Home;
